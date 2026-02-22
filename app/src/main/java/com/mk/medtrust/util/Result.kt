a00ca0621e6f2bce4529022f8d058a027a1c2102
Manash396 <manasash26@gmail.com>
2026-02-22 12:26:44 +0530

    added correct base64 file

--- app/src/main/java/com/mk/medtrust/util/Result.kt
+++ app/src/main/java/com/mk/medtrust/util/Result.kt
@@ -1,7 +1,7 @@
 package com.mk.medtrust.util
 
 sealed class Result<out T> {
-
+// Okay Krishna
     data class Success<T>(val data: T) : Result<T>()
 
     data class Error<T>(val message: String, val data: T? = null) : Result<T>()
